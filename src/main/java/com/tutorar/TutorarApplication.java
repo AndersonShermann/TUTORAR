0d13e2f69e1472987fb990182f526a4a26bd2bb5
Anderson Borges de Oliveira (Shermann) <andersonshermann@gmail.com>
2023-11-13 13:55:28 -0300

    Initial commit

---
+++ src/main/java/com/tutorar/TutorarApplication.java
@@ -0,0 +1,13 @@
+package com.tutorar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorar/TutorarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
