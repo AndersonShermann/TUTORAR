e65685667bfe85a227aa234c2037f8b4b111cc6d
Anderson Borges de Oliveira (Shermann) <andersonshermann@gmail.com>
2023-11-14 16:42:47 -0300

    model, view, controller e repository do usuario

---
+++ src/main/java/com/tutorar/controller/UsuarioController.java
@@ -0,0 +1,59 @@
+package com.tutorar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tutorar.model.Usuario;
+import com.tutorar.repository.UsuarioRepository;
+
+@Controller
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("usuario/home");
+
+        modelAndView.addObject("usuarios", usuarioRepository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("usuario/cadastro");
+
+        modelAndView.addObject("usuario", new Usuario());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("usuario/formulario");
+
+        modelAndView.addObject("usuario", usuarioRepository.getReferenceById(id));
+
+        return modelAndView;
+    }
+
+    @PostMapping({"/cadastrar", "/{id}/editar"})
+    public String salvar(Usuario usuario) {
+        usuarioRepository.save(usuario);
+
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+        usuarioRepository.deleteById(id);
+
+        return "redirect:/usuarios";
+    }
+}
