435573013be00a588835568e03c38b49932954cf
Anderson Borges de Oliveira (Shermann) <andersonshermann@gmail.com>
2023-11-18 11:42:37 -0300

    modelagem atualizada

--- src/main/java/com/tutorar/controller/UsuarioController.java
+++ src/main/java/com/tutorar/controller/UsuarioController.java
@@ -20,7 +20,7 @@ public class UsuarioController {
     
 	@GetMapping("/listar-usuario")
 	public ModelAndView listar() {
-		ModelAndView modelAndView = new ModelAndView("usuario/listar.html");
+		ModelAndView modelAndView = new ModelAndView("usuario/listarUsuario.html");
  
 		List<Usuario> usuarios = usuarioRepository.findAll();
 		modelAndView.addObject("usuario", usuarios);
@@ -28,20 +28,10 @@ public class UsuarioController {
 		return modelAndView;
 	}
 	
-	
-
-//    @GetMapping()
-//    public ModelAndView home() {
-//        ModelAndView modelAndView = new ModelAndView("usuario/perfil");
-//
-//        modelAndView.addObject("usuarios", usuarioRepository.findAll());
-//
-//        return modelAndView;
-//    }
 
     @GetMapping("/cadastrar-usuario")
     public ModelAndView cadastrar() {
-        ModelAndView modelAndView = new ModelAndView("usuario/cadastro");
+        ModelAndView modelAndView = new ModelAndView("usuario/cadastroUsuario");
 
         modelAndView.addObject("usuario", new Usuario());
 
@@ -50,7 +40,7 @@ public class UsuarioController {
 
     @GetMapping("/{id}/editar-usuario")
     public ModelAndView editar(@PathVariable Long id) {
-        ModelAndView modelAndView = new ModelAndView("usuario/editar");
+        ModelAndView modelAndView = new ModelAndView("usuario/editarUsuario");
 
         modelAndView.addObject("usuario", usuarioRepository.getReferenceById(id));
 
@@ -68,12 +58,12 @@ public class UsuarioController {
     public String excluir(@PathVariable Long id) {
         usuarioRepository.deleteById(id);
 
-        return "redirect:/listar";
+        return "redirect:/listar-usuario";
     }
     
 	@GetMapping("/{id}/perfil-usuario")
 	public ModelAndView perfil(@PathVariable Long id) {
-		ModelAndView modelAndView = new ModelAndView("usuario/perfil");
+		ModelAndView modelAndView = new ModelAndView("usuario/perfilUsuario");
  
 		Usuario usuario = usuarioRepository.getReferenceById(id);
 		modelAndView.addObject("usuario", usuario);
