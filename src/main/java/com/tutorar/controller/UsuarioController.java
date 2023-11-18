69c405ccd1e9ae9dc17f9fa7538f6b2c22b31067
Anderson Borges de Oliveira (Shermann) <andersonshermann@gmail.com>
2023-11-18 12:26:29 -0300

    index e controller-rota principal

--- src/main/java/com/tutorar/controller/UsuarioController.java
+++ src/main/java/com/tutorar/controller/UsuarioController.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tutorar.model.Usuario;
 import com.tutorar.repository.UsuarioRepository;
 
 @Controller
+@RequestMapping("/")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    
     
 	@GetMapping("/listar-usuario")
 	public ModelAndView listar() {
